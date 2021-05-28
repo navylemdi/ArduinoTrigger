bde895b4b07364b62eb90eb9547f97452b6f219f
DILEM Yvan <yvan.dilem.1@ens.etsmtl.ca>
2021-05-28 07:59:29 -0400

    Ajout calcul microseconde et rapport cyclique

--- ArduinoTrigger.ino
+++ ArduinoTrigger.ino
@@ -7,18 +7,28 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
+
+  float alpha=0.5; //rapport cyclique
   float fps=20; //Choose fps of camera
   int sensorValue=analogRead(A1); //recuperation valeur A1
   float Voltage=5.0*sensorValue/1023.0; //Conversion en Volt
-  Serial.println(Voltage); // Affichage voltage
+  
+  Serial.print(Voltage); // Affichage voltage
+  Serial.println(" V");
+  
+  unsigned long StartTime = micros();
+  
   if (Voltage >= 3.0){
-    
+    unsigned long CurrentTime = micros();
+    unsigned long ElapsedTime = CurrentTime - StartTime;
+    Serial.print(ElapsedTime);
+    Serial.println(" microsecond");
     digitalWrite(A0, HIGH);   // send the trigger
     digitalWrite(LED_BUILTIN, HIGH);// turn the LED on (HIGH is the voltage level)
-    delay(0.5*1000/fps);      // wait
+    delay(alpha*1000.0/fps);      // wait
     digitalWrite(A0, LOW);    // drop the trigger
     digitalWrite(LED_BUILTIN, LOW);// turn the LED off by making the voltage LOW
-    delay(0.5*1000/fps);      // wait
+    delay((1.0-alpha)*1000.0/fps);      // wait
   }
   
 }
