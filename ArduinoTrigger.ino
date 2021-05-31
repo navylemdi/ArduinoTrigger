4e25c645591c923f4c2ffe5a66cb540ba3d61cd4
DILEM Yvan <yvan.dilem.1@ens.etsmtl.ca>
2021-05-31 16:16:54 -0400

    Update ArduinotTrigger.ino

--- ArduinoTrigger.ino
+++ ArduinoTrigger.ino
@@ -8,8 +8,8 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
 
-  float alpha=0.5; //rapport cyclique
-  float fps=20; //Choose fps of camera
+  float alpha=0.05; //rapport cyclique
+  float fps=2; //Choose fps of camera
   int sensorValue=analogRead(A1); //recuperation valeur A1
   float Voltage=5.0*sensorValue/1023.0; //Conversion en Volt
   
@@ -18,17 +18,19 @@ void loop() {
   
   unsigned long StartTime = micros();
   
-  if (Voltage >= 3.0){
+  if (Voltage >= 2.95){
     unsigned long CurrentTime = micros();
     unsigned long ElapsedTime = CurrentTime - StartTime;
     Serial.print(ElapsedTime);
     Serial.println(" microsecond");
+    
     digitalWrite(A0, HIGH);   // send the trigger
     digitalWrite(LED_BUILTIN, HIGH);// turn the LED on (HIGH is the voltage level)
     delay(alpha*1000.0/fps);      // wait
     digitalWrite(A0, LOW);    // drop the trigger
     digitalWrite(LED_BUILTIN, LOW);// turn the LED off by making the voltage LOW
     delay((1.0-alpha)*1000.0/fps);      // wait
+    //exportation dans un .txt
   }
   
 }
