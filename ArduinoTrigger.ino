85a5f90712ee55f047d339d2b10eada54dee08b8
DILEM Yvan <yvan.dilem.1@ens.etsmtl.ca>
2021-06-11 14:16:26 -0400

    Add choice if calibration or not

--- ArduinoTrigger.ino
+++ ArduinoTrigger.ino
@@ -12,13 +12,14 @@ void loop() {
   float fps=2; //Choose fps of camera
   int sensorValue=analogRead(A1); //recuperation valeur A1
   float Voltage=5.0*sensorValue/1023.0; //Conversion en Volt
+  boolean Calibration = true;
   
   Serial.print(Voltage); // Affichage voltage
   Serial.println(" V");
   
   unsigned long StartTime = micros();
   
-  if (Voltage >= 2.95){
+  if (Voltage >= 2.95 and Calibration == false){
     unsigned long CurrentTime = micros();
     unsigned long ElapsedTime = CurrentTime - StartTime;
     Serial.print(ElapsedTime);
@@ -30,7 +31,15 @@ void loop() {
     digitalWrite(A0, LOW);    // drop the trigger
     digitalWrite(LED_BUILTIN, LOW);// turn the LED off by making the voltage LOW
     delay((1.0-alpha)*1000.0/fps);      // wait
-    //exportation dans un .txt
   }
+  else if (Calibration = true){
+    digitalWrite(A0, HIGH);   // send the trigger
+    digitalWrite(LED_BUILTIN, HIGH);// turn the LED on (HIGH is the voltage level)
+    delay(alpha*1000.0/fps);      // wait
+    digitalWrite(A0, LOW);    // drop the trigger
+    digitalWrite(LED_BUILTIN, LOW);// turn the LED off by making the voltage LOW
+    delay((1.0-alpha)*1000.0/fps);      // wait
+  }
+ 
   
 }
